777c776e1d44f04baf39ad0103632e998df49c01
MMustafa <mmustafayev0001@gmail.com>
2023-04-24 18:50:42 +0400

    user implemented

---
+++ Producer/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            return Ok();
+        }
+    }
+}
